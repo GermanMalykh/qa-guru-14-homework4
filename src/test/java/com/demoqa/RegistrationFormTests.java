546704268a435321dbdb651d3e37e35d87067b00
malykh.gn <germanmalyh@gmail.com>
2022-08-02 21:58:00 +0300

    refactoring imports

--- src/test/java/com/demoqa/RegistrationFormTests.java
+++ src/test/java/com/demoqa/RegistrationFormTests.java
@@ -1,15 +1,13 @@
 package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormTests {
 
